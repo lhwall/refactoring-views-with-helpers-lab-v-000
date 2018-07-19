5e234040b399a84d4c47c481f832fab00d19ff96
Lucy Wall <lhwall@gmail.com>
2018-07-19 01:15:48 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,12 @@
 module SongsHelper
 
 def display_artist(song)
-  if !!song.artist 
+  if !!song.artist
     link_to song.artist.name, artist_path(song.artist)
   else
-    link to "Add Artist", edit_song_path(song) 
+    link to "Add Artist", edit_song_path(song)
   end
-end 
+end
 
 
 
